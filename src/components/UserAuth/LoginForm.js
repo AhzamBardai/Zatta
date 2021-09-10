133ea2e4e515917c194a02508bddd7bc28296313
tvergara <tverg419@gmail.com>
2021-09-09 23:29:28 -0400

    password modal

--- src/components/UserAuth/LoginForm.js
+++ src/components/UserAuth/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios'
 import userStore  from '../Users/GetUsers';
+import PasswordModal from './PasswordModal';
 
 function LoginForm({ history }) {
 
@@ -14,6 +15,7 @@ function LoginForm({ history }) {
     const setTodos = userStore(state => state.setTodos) 
     const setLogedIn = userStore(state => state.setLoggedIn)
     const setUser = userStore(state => state.setCurrentUser)
+    const [modal, setModal] = useState(false)
 
     const [loginInfo, setLoginInfo] = useState({
         username: '',
@@ -55,7 +57,7 @@ function LoginForm({ history }) {
 
                     }  else {
                         setLogedIn(false)
-                        window.alert('Incorrect username or password')
+                        setModal(true)
                     }
                 })
                 
@@ -107,6 +109,7 @@ function LoginForm({ history }) {
                 Create an Account
             </Link>
         </div>
+        { modal && <PasswordModal modal={modal} setModal={setModal}/> }
     </form>
 )
 }
---
+++ src/components/UserAuth/PasswordModal.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Modal from 'react-modal';
+import { Button } from 'react-bootstrap';
+
+
+function PasswordModal({ modal, setModal }) {
+
+    const modalStyle = {
+        content: {
+          top: '35%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 2
+        },
+      };
+
+
+    function closeModal() {
+        setModal(false)
+    }
+
+
+    return (
+        <div>
+        <Modal isOpen= {modal} onRequestClose={() => setModal(false)} style={modalStyle}>
+            <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+                <h3>Username or Password Incorrect</h3>
+                <Button style={{marginTop:"10px",width:"5rem", backgroundColor:"black"}} variant="dark" onClick={closeModal}>Close</Button>
+            </div>
+        </Modal>
+    </div>
+)
+}
+
+export default PasswordModal
